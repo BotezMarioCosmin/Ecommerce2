23663d4f622ddf9008b5cb368412560405be048e
Botez Mario Cosmin <botezmariocosmin@gmail.com>
2023-01-30 08:25:38 +0100

    Add project files.

---
+++ Ecommerce2/Cart.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce2
+{
+    public class Cart
+    {
+        private string _id; //primary key
+        private int _numeroprodotti = 0;
+        private Product[] prodotti;
+
+        public Cart(string id1, Product[] arrayp)
+        {
+            Id = id1;
+            prodotti = arrayp;
+        }
+
+        public string Id
+        {
+            set { _id = value; }
+            get { return _id; }
+        }
+
+        public void aggiungi(Product p)
+        {
+            prodotti[_numeroprodotti] = p;
+            _numeroprodotti++;
+        }
+
+        public Product rimuovi(Product p)
+        {
+            for (int i = 0; i < _numeroprodotti; i++)
+            {
+                if (p == prodotti[i])
+                {
+                    prodotti[i] = null;
+                    return p;
+                }
+            }
+            throw new Exception("prodotto insesistente");
+        }
+
+        public Product[] svuota()
+        {
+            Product[] array = new Product[999];
+            for (int i = 0; i < _numeroprodotti; i++)
+            {
+                array[i] = prodotti[i];
+                prodotti[i] = null;
+            }
+            return array;
+        }
+
+        public Product[] getProdotti()
+        {
+            return prodotti;
+        }
+    }
+}
---
+++ Ecommerce2/Product.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce2
+{
+    public class Product
+    {
+        private string _id; //primary key
+        private string _nome;
+        private float _prezzo;
+        private string _produttore;
+        private string _descrizione;
+
+        public Product(string id1, string nome1, float prezzo1, string produttore1, string descrizione1)
+        {
+            Id = id1;
+            Nome = nome1;
+            Prezzo = prezzo1;
+            Produttore = produttore1;
+            Descrizione = descrizione1;
+        }
+
+        public string Id
+        {
+            set { _id = value; }
+            get { return _id; }
+        }
+
+        public string Nome
+        {
+            set { _nome = value; }
+            get { return _nome; }
+        }
+
+        public float Prezzo
+        {
+            set { _prezzo = value; }
+            get { return _prezzo; }
+        }
+
+        public string Produttore
+        {
+            set { _produttore = value; }
+            get { return _produttore; }
+        }
+
+        public string Descrizione
+        {
+            set { _descrizione = value; }
+            get { return _descrizione; }
+        }
+
+        public string toStringId()
+        {
+            return Id;
+        }
+
+        public string toStringNome()
+        {
+            return Nome;
+        }
+
+        public float toStringPrezzo()
+        {
+            return Prezzo;
+        }
+
+        public string toStringProduttore()
+        {
+            return Produttore;
+        }
+
+        public string toStringDescrizione()
+        {
+            return Descrizione;
+        }
+    }
+}
